c0b16cbd70202b46d209263d2576513549839dd3
siavash <siavash@siavashs-iMac.local>
2014-08-24 09:30:37 +0200

    added visualization of t quantile function

--- TIntervalls.R
+++ TIntervalls.R
@@ -1,5 +1,18 @@
 library(manipulate)
 library(ggplot2)
+
+k <- 1000
+xvals <- seq(-5, 5, length = k)
+myplot <- function(df){
+  d <- data.frame(y = c(dnorm(xvals), dt(xvals, df)),
+                  x = xvals,
+                  dist = factor(rep(c("Normal", "T"), c(k,k))))
+  g <- ggplot(d, aes(x = x, y = y)) 
+  g <- g + geom_line(size = 2, aes(colour = dist))
+  g
+}
+manipulate(myplot(mu), mu = slider(1, 20, step = 1)) 
+
 pvals <- seq(.5, .99, by = .01)
 myplot2 <- function(df){
   d <- data.frame(n= qnorm(pvals),t=qt(pvals, df),
